5e7d7db1f7b9d24d7f2da65b9ffa91eba7f89616
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2022-11-30 18:56:15 +0300

    added properties for populate db

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,17 @@
+insert into Sensor (name, rain, temperature)
+values ('Датчик Шереметьево', false, -5.65);
+
+insert into Sensor (name, rain, temperature)
+values ('Датчик Внуково', false, -4.32);
+
+insert into Sensor (name, rain, temperature)
+values ('Датчик Домодедово', false, -6.84);
+
+insert into Sensor (name, rain, temperature)
+values ('Датчик Phuket', true, 28.1);
+
+insert into Sensor (name, rain, temperature)
+values ('Датчик Antalya', true, 22.0);
+
+insert into Sensor (name, rain, temperature)
+values ('Датчик Hurghada', false, 32);
