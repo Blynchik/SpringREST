92b0207b8a43bf5c8ed8983bd84e07c60944c6d5
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2022-12-01 18:51:11 +0300

    added tables for measuremant

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,17 +1,28 @@
-insert into Sensor (name)
+insert into sensor (name)
 values ('Датчик Шереметьево');
 
-insert into Sensor (name)
+insert into sensor (name)
 values ('Датчик Внуково');
 
-insert into Sensor (name)
+insert into sensor (name)
 values ('Датчик Домодедово');
 
-insert into Sensor (name)
+insert into sensor (name)
 values ('Датчик Phuket');
 
-insert into Sensor (name)
+insert into sensor (name)
 values ('Датчик Antalya');
 
-insert into Sensor (name)
+insert into sensor (name)
 values ('Датчик Hurghada');
+
+insert into measurement (value, raining, sensor_id)
+values (27.8, false, 2);
+insert into measurement (value, raining, sensor_id)
+values (24.78, true, 5);
+insert into measurement (value, raining, sensor_id)
+values (22.349, false, 3);
+insert into measurement (value, raining, sensor_id)
+values (21.0, true, 1);
+insert into measurement (value, raining, sensor_id)
+values (31, false, 6);
