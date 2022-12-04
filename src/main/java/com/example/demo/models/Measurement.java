559f7dba32063abfbe49216e6df6f567db52a38b
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2022-12-04 16:09:59 +0300

    constructor and getters/seters added to measuremnet

--- src/main/java/com/example/demo/models/Measurement.java
+++ src/main/java/com/example/demo/models/Measurement.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 
+import java.util.Objects;
+
 @Entity
 @Table (name = "Measurement")
 public class Measurement {
@@ -27,4 +29,58 @@ public class Measurement {
     @ManyToOne
     @JoinColumn(name = "sensor_id", referencedColumnName = "id")
     private Sensor owner;
+
+    public Measurement(){
+    }
+
+    public Measurement(double value, boolean raining, Sensor owner){
+        this.value = value;
+        this.raining = raining;
+        this.owner = owner;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public boolean isRaining() {
+        return raining;
+    }
+
+    public void setRaining(boolean raining) {
+        this.raining = raining;
+    }
+
+    public Sensor getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Sensor owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Measurement that = (Measurement) o;
+        return id == that.id && Double.compare(that.value, value) == 0 && raining == that.raining && Objects.equals(owner, that.owner);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value, raining, owner);
+    }
 }
